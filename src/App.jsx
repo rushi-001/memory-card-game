57129f1cb43635495370032eca11298a5ec1a15b
RUSHI PANCHAL <133478781+rushi-001@users.noreply.github.com>
2024-04-14 13:36:20 +0530

    new project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div id="test" className="font-bold text-white bg-black text-3xl">
+        hello
+      </div>
+    </>
+  );
+}
+
+export default App;
